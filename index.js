e730575b60b2121fb4cff3e4cb75bd095cb61c5d
davidmota95 <davidrafaelmotta@gmail.com>
2021-06-17 08:32:44 -0300

    cira lista e botão de apagar

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ btnadd.addEventListener("click", adicionar);
 function adicionar() {
   if (nome.value == "" || preco.value <= 0)
     window.alert("[erro] campo nao preenchido");
+  else {
+    let ulista = window.document.getElementById("ulista");
+    let lista = window.document.createElement("li");
+    let btndell = window.document.createElement("button");
+    btndell.textContent = "Apagar";
+
+    btndell.addEventListener("click", deletar);
+    function deletar() {
+      lista.remove();
+    }
+
+    ulista.appendChild(lista);
+    lista.innerHTML = `Produto : ${nome.value} <p>Preço :  R$${preco.value}`;
+    lista.appendChild(btndell);
+  }
+  nome.value = ``;
+  preco.value = ``;
 }
